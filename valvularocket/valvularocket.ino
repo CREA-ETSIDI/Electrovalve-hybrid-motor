6df56fc187f60aa6c1d09f8e394da747420589d9
Marcelo <47826132+mlunad88@users.noreply.github.com>
2022-10-20 00:40:25 +0200

    Add files via upload

--- valvularocket/valvularocket.ino
+++ valvularocket/valvularocket.ino
@@ -13,31 +13,36 @@ void setup() {
   pinMode (3, INPUT);// pin opcional entrada botón 2
 
   //declaramos estado inicial
-  digitalWrite(9, LOW);
-  delay(1000);
+  digitalWrite(9, LOW);//pin de la válvula
 
   //Algo de info por serial
   Serial.begin(9600);
   Serial.println("Arrancando motores...");
+  delay(1000);
 }
 
 void loop () { 
-  botonera();
-  //tempura();
+  //botonera();
+  tempura();
+  //encendido();
 }
 
 //Opción funcionamiento botonera
 void botonera(void){
-  if (digitalRead (2) == HIGH) 
-{ 
-     Serial.println("ESTAMOS ON");
-     digitalWrite (9, HIGH); 
-     delay(1000); 
-} 
-else if (digitalRead (3) == HIGH) 
+  boolean apertura;
+  boolean cierre;
+  apertura=digitalRead(2);
+  cierre=digitalRead(3);
+  if (apertura && cierre) 
 { 
      Serial.println("ESTAMOS OFF");
      digitalWrite (9, LOW); 
+     delay(1000); 
+} 
+else if(apertura)
+{ 
+     Serial.println("ESTAMOS ON");
+     digitalWrite (9, HIGH); 
      delay(1000); 
 } 
 }
@@ -50,4 +55,13 @@ void tempura(void){
   digitalWrite(9, LOW);
   Serial.println("ESTAMOS OFF");
   delay(1000);
+}
+//Opción siempre on
+void encendido(void){
+  digitalWrite(9, HIGH);
+  Serial.println("ESTAMOS ON");
+  delay(1000);
+}
+void input(void){
+  
 }
